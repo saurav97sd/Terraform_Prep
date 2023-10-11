30e625690e62fa4a766d608a1a273e2be77b96a2
Saurav Das <sauravdas969.sd@gmail.com>
2023-10-11 19:05:29 +0530

    introduction to terraform remote state storage and state locking

---
+++ 006_terraform_state/01_terraform_remote_state_storage_&_state_locking/c1_versions.tf
@@ -0,0 +1,27 @@
+# Terraform Block (CLI Locking)
+terraform {
+  required_version = "~> 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  # Adding backend as S3 for Remote State Storage
+  # https://developer.hashicorp.com/terraform/language/settings/backends/s3
+  backend "s3" {
+    bucket = "terraform-remotestate001"
+    key    = "dev/terraform.tfstate" # dev is the folder which we created after creating S3 bucket in AWS portal
+    region = "us-east-1"
+    # For state locking
+    dynamodb_table = "terraform-dev-state-locking-table"
+  }
+
+}
+
+# Provider Block (Used to initialize the region and provide credentials)
+provider "aws" {
+  region  = var.aws_region
+  profile = "default"
+}
